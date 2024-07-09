bc8139c394fd3545368fe2d49ad4ac1c4f8fa9a9
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-07-09 09:38:07 +0530

    custom attribute directives

--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MenuButtonComponent } from './menu-button/menu-button.component';
 import { ItemCardComponent } from './item-card/item-card.component';
+import { HighlightDirective } from './directives/highlight-directive';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ItemCardComponent } from './item-card/item-card.component';
     NavBarComponent,
     MenuButtonComponent,
     ItemCardComponent,
+    HighlightDirective,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
