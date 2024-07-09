d650e37d1760ad0d86f3c6468768c1661a05be85
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-07-09 15:46:47 +0530

    httpClient impot

--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -21,7 +22,7 @@ import { UnitPipePipe } from './pipes/unit-pipe.pipe';
     HighlightDirective,
     UnitPipePipe,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [ItemService],
   bootstrap: [AppComponent],
 })
