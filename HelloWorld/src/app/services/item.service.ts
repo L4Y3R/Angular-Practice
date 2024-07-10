7131da494efe5943f0a1aa39a48c7a6254eaae87
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-07-10 09:33:37 +0530

    error handling for http request

--- HelloWorld/src/app/services/item.service.ts
+++ HelloWorld/src/app/services/item.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Item } from '../data-models/items';
 import { HttpClient } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ItemService {
-  listItemUrl = 'assets/be-server-mocks/items.json';
+  listItemUrl = 'assets/be-server-mocks/itemss.json';
   constructor(private http: HttpClient) {}
 
   getItemList() {
-    return this.http.get<Item[]>(this.listItemUrl, { observe: 'response' });
+    const observe = { observe: 'response' as const };
+    return this.http
+      .get<Item[]>(this.listItemUrl, observe)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  private handleError(err, caught): Observable<any> {
+    if (err.status === 0) {
+      //client side error
+      console.error('An error occured in client side: ', err.error);
+    } else {
+      //server side error
+      console.error(
+        'An error occure in server side: ',
+        err.error,
+        'Status code: ',
+        err.status
+      );
+    }
+    throw 'An error occured, try again';
   }
 }
